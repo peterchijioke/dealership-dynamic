d5eee2e5aa3dc330eda19c69682501340d7c2296
Ifeanyi Nnorom <nnoromifeanyi@gmail.com>
2025-09-03 11:50:27 +0100

    fix: infinite hit

--- app/(routes)/[...slug]/page copy.tsx
+++ app/(routes)/[...slug]/page copy.tsx
@@ -49,7 +49,7 @@ export default async function CatchAllPage({ params, searchParams }: PageProps) 
 
     return (
         <div className="h-screen flex flex-col relative">
-            <SearchClient initialResults={initialResults} facetFilters={facetFilters} />
+            {/* <SearchClient initialResults={initialResults} facetFilters={facetFilters} /> */}
         </div>
     );
 }
