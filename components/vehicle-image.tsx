4c3e9132fc0b3d37386f85bae8c76c47a35e7a91
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-09-14 15:24:07 +0100

    feat: fix

--- components/vehicle-image.tsx
+++ components/vehicle-image.tsx
@@ -13,6 +13,7 @@ interface VehicleImageProps {
 }
 const blurDataURL =
   "https://dealertower.app/image/UuPD13gL_j3TIE57a2mSoOGLRxwnwl_PpZ0OOPDuQ1icAbVhR_62E7sfpRP7217gmQmnZGk2UQ_YGUIwA-8OVVbNXjhM1nntORyi1OhKFWu6LD2kslrksibGGIkol0mk7spFpJbIja2z0mpjuB2FLux6aOZKzArQNHRnVWXp3omIvBsiM1G9qOu0koZP06vG0mGrvanG1c6TVvPaihCMs2zfkzelz9ls_6cLDmKxDF05-fr642YaI6rrC9rtCQ.avif";
+
 export default function VehicleImage({
   hit,
   encryptedUrl,
