285dc9ca0774daa8fb5cc044e619b11dc0f1adaa
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-09-22 18:35:23 +0100

    Update NavigationMenu.tsx

--- components/layouts/NavigationMenu.tsx
+++ components/layouts/NavigationMenu.tsx
@@ -49,7 +49,7 @@ const NavigationMenuComponent = ({
                     )}
                   >
                     <Link href={`/inventory/${item.link}`}>
-                      {item.label.toLowerCase()}
+                      {item.label.toUpperCase()}
                     </Link>
                   </NavigationMenuTrigger>
                   <NavigationMenuContent>
@@ -139,7 +139,7 @@ const NavigationMenuComponent = ({
                 <>
                   <NavigationMenuTrigger className="bg-transparent rounded-none cursor-pointer hover:bg-transparent font-semibold px-4 py-6 uppercase">
                     <Link prefetch={true} href={item.link}>
-                      {item.label.toLowerCase()}
+                      {item.label.toUpperCase()}
                     </Link>
                   </NavigationMenuTrigger>
                   <NavigationMenuContent className="uppercase">
@@ -150,9 +150,9 @@ const NavigationMenuComponent = ({
                             <Link
                               prefetch={true}
                               href={child.link}
-                              className="block select-none rounded-md p-2 leading-none no-underline outline-none transition-colors hover:bg-transparent hover:text-accent-foreground focus:bg-transparent focus:text-accent-foreground bg-transparent text-base font-medium underline-offset-2 hover:underline"
+                              className="block select-none uppercase rounded-md p-2 leading-none no-underline outline-none transition-colors hover:bg-transparent hover:text-accent-foreground focus:bg-transparent focus:text-accent-foreground bg-transparent text-base font-medium underline-offset-2 hover:underline"
                             >
-                              {child.label}
+                              {child.label.toUpperCase()}
                             </Link>
                           </NavigationMenuLink>
                         </li>
@@ -162,11 +162,11 @@ const NavigationMenuComponent = ({
                 </>
               ) : (
                 <NavigationMenuLink
-                  className={`${navigationMenuTriggerStyle()} bg-transparent hover:bg-transparent font-semibold px-4 py-3 capitalize`}
+                  className={`${navigationMenuTriggerStyle()} bg-transparent hover:bg-transparent font-semibold px-4 py-3 uppercase`}
                   asChild
                 >
                   <Link prefetch={true} href={item?.link}>
-                    {item?.label.toLowerCase()}
+                    {item?.label.toUpperCase()}
                   </Link>
                 </NavigationMenuLink>
               )}
