973add4c7c48800a9dce4bfafb119f6bf2ee8405
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-09-23 13:14:58 +0100

    fix: update

--- components/inventory/VideoPlayer.tsx
+++ components/inventory/VideoPlayer.tsx
@@ -66,7 +66,7 @@ function VideoPlayer({ video, videoCc, poster }: Props) {
         preload="none"
         disablePictureInPicture
         className="absolute inset-0 w-full h-full object-cover"
-        style={{ display: "block" }}
+        style={{ display: "block", width: "100%", height: "100%" }}
       >
         <source src={video} />
         {videoCc && (
